b3707d6e4acb4c7cf857050f96fbef72345b0ace
Roxx90 <rnovi.profil@gmail.com>
2025-07-06 18:46:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const songs = [
-  {
-    title: "2 Brothers On The 4th Floor - Dreams",
-    url: "https://docs.google.com/uc?export=download&id=1i3BNexCZJoF817Z0wduX5XcctT5p-Dlo"
-  },
-  {
-    title: "2 Brothers on the 4th Floor - Let Me Be Free ",
-    url: "https://docs.google.com/uc?export=download&id=1QICbsrOkxheuc2eWYNIpng9TXE6IJtSN"
-  },
-  {
-    title: "2 Unlimited - Get Ready For This",
-    url: "https://docs.google.com/uc?export=download&id=1k-VBr2bzzhXjlPHnLopR8DDp9JUaf_Xz"
-  }
+  const songs = [
+  { 2 Brothers On The 4th Floor - Dreams: "Track 1", url: "https://docs.google.com/uc?export=download&id=11wwaZANJuNHSR6NXxXtbqOgKCPka0RXo" },
+  { 2 Brothers on the 4th Floor - Let Me Be Free: "Track 2", url: "https://docs.google.com/uc?export=download&id=144k4w-hG0VpWaX7MmZraoOeiGVUBGp81" },
+  { 2 Unlimited - Get Ready For This: "Track 3", url: "https://docs.google.com/uc?export=download&id=1ARMgrAcMZxyJBTHPNT_L-Fnyk0O5RT__" },
+  { title: "Track 4", url: "https://docs.google.com/uc?export=download&id=1B9jTFBSEXYVOrKn-wt_2q9YtL14dQop2" },
+  { title: "Track 5", url: "https://docs.google.com/uc?export=download&id=1JjHEC0Hm9i44MhsfHrGx4Nh_GRFDqfwc" },
+  { title: "Track 6", url: "https://docs.google.com/uc?export=download&id=1QICbsrOkxheuc2eWYNIpng9TXE6IJtSN" },
+  { title: "Track 7", url: "https://docs.google.com/uc?export=download&id=1i3BNexCZJoF817Z0wduX5XcctT5p-Dlo" },
+  { title: "Track 8", url: "https://docs.google.com/uc?export=download&id=1k-VBr2bzzhXjlPHnLopR8DDp9JUaf_Xz" },
+  { title: "Track 9", url: "https://docs.google.com/uc?export=download&id=1s_f3CIL-dPs5_8LfYxbvdDJNQJ33oKon" },
+  { title: "Track 10", url: "https://docs.google.com/uc?export=download&id=1xvuM34SwpwWY3K3k6D8do_8IYNbGgrMd" }
 ];
+
 
 let currentIndex = 0;
 let randomMode = false;
